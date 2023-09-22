623ee343256bc4e17435b3f7793df0396c62dc0b
dary-winata <dary.winata15@gmail.com>
2023-09-22 18:13:39 +0700

    Initial Commit

---
+++ LearningCoreNFC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningCoreNFC
+//
+//  Created by dary winata nugraha djati on 22/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
