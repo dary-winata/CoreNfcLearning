7222cb8f757df4719d576b8dc7df12adb4efbec1
dary-winata <dary.winata15@gmail.com>
2024-01-09 13:59:45 +0700

    just reading

--- LearningCoreNFC/ViewController.swift
+++ LearningCoreNFC/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+    
+    @IBOutlet weak var textTf: UILabel!
+    var session : NFCNDEFReaderSession?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        DispatchQueue.main.async {
+            self.textTf.text = messages.description
+            print(messages)
+        }
+    }
+    
+    @IBAction func beginScanning(_ sender: Any) {
+        guard NFCNDEFReaderSession.readingAvailable else {
+            let alertController = UIAlertController(
+                title: "Scanning not supported", message: "This device doesn't support tag scanning.", preferredStyle: .alert
+            )
+            alertController.addAction(UIAlertAction(title: "Ok", style: .default))
+            self.present(alertController, animated: true, completion: nil)
+            return
+        }
+        session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
+        session?.alertMessage = "Hold your iPhone near the item to learn more about it."
+        session?.begin()
 
+    }
 }
 
